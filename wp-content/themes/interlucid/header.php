8f91276baa7405e66e031a33ce4426bcac8acd46
Brandon der Blätter <intcreator@gmail.com>
2019-03-23 14:06:57 -0600

    Clean a few things from Understrap

--- wp-content/themes/interlucid/header.php
+++ wp-content/themes/interlucid/header.php
@@ -18,7 +18,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
